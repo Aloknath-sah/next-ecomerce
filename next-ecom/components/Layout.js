62b3ac3a155bada40694195e93636f36f90fbacc
Aloknath-sah <kumaraditya6396@gmail.com>
2022-01-18 17:10:58 +0530

    material ui styles implemented in homepage

--- next-ecom/components/Layout.js
+++ next-ecom/components/Layout.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar,Toolbar, Typography, Container } from '@material-ui/core';
+import useStyles from '../utils/styles';
 
-export const Layout = () => {
+export const Layout = ({children}) => {
+    const classes = useStyles();
     return (
         <div>
             <Head>
                 <title>next ecommerce</title>
             </Head>
-            <AppBar position="static">
+            <AppBar position="static" className={classes.navbar} >
                 <Toolbar>
                     <Typography>Ecom</Typography>
                 </Toolbar>
             </AppBar>
-            <Container>
+            <Container className={classes.main}>
                 {children}
             </Container>
+            <footer className={classes.footer}>
+                <Typography>All rights reserved.</Typography>
+            </footer>
         </div>
     )
 }
--- next-ecom/pages/index.js
+++ next-ecom/pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Layout } from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <Layout>
+      <div>
+        <h1>Products</h1>
+      </div>
+    </Layout>
   )
 }
